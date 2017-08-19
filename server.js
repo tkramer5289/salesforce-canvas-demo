0906da4ac6f92ff6e9b0cbea25f277e0b0b658f0
lightningexperience <lightningexperience@users.noreply.github.com>
2017-08-18 23:54:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ var express = require('express'),
 
 var router = express.Router();
 var path = __dirname + '/views/';
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
+
 router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
