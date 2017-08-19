bdcee6f3fbcf12ef339c6373f7c7b86b7b5d10b2
lightningexperience <lightningexperience@users.noreply.github.com>
2017-08-18 21:48:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ app.post('/signedrequest', function(req, res) {
     });
 
 });
-
+app.get('/', function (req, res) {
+  res.end('Hi there!')
+})
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
